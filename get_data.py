8285f9f3a3d40576f5c350b8a3b96c5974ab54f7
Hoang Long <litpuvn@gmail.com>
2019-03-30 13:54:38 -0500

    ignore dist file and hide token

--- get_data.py
+++ get_data.py
@@ -1,6 +1,6 @@
 from noaa_api_v2 import NOAAData
 
-api_token = "fUvaoXWEbnuHzrHppXOKoDgLlZMxNCmO"
+api_token = "your token"
 data = NOAAData(api_token)
 
 categories = data.data_categories(locationid='FIPS:37', sortfield='name')
