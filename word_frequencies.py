71a2acc162bee4cd186ed8aa315cff2be71edbaa
Hoang Long <litpuvn@gmail.com>
2019-04-03 17:24:45 -0500

    adding top words frequency

---
+++ word_frequencies.py
@@ -0,0 +1,39 @@
+import collections
+import csv
+
+
+def top_words(text, count=100):
+    counts = collections.Counter(text.split())
+    return counts.most_common(5)
+
+
+filepath = 'data/harvey/harvey_2017_08_17.csv'
+tweet_by_city = dict()
+with open(filepath) as filepointer:
+    text_reader = csv.reader(filepointer, delimiter=',')
+
+    for index, row in enumerate(text_reader):
+        if index < 1:
+            continue
+
+        tweet_date = row[1]
+        tweet_hour = row[2]
+        tweet_minute = row[3]
+
+        city = row[4].lower()
+        text = row[6]
+
+        if city not in tweet_by_city:
+            tweet_by_city[city] = []
+
+        word_list = tweet_by_city[city]
+        word_list = word_list + text.split()
+
+    city_top_words = dict()
+    for city, words in tweet_by_city.items():
+        top_words = top_words(words)
+        city_top_words[city] = top_words
+
+        print("City", city, ":top words:", top_words)
+
+
