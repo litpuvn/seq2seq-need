58606a3a9201a3168546da64893f3a9b371a225d
Hoang Long <litpuvn@gmail.com>
2019-05-23 15:02:10 -0500

    add lstm generation

--- get_data.py
+++
@@ -1,9 +0,0 @@
-from noaa_api_v2 import NOAAData
-
-api_token = "your token"
-data = NOAAData(api_token)
-
-categories = data.data_categories(locationid='FIPS:37', sortfield='name')
-
-for i in categories:
-    print(i)
