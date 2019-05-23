74cdff4f35ff20f2b28986855d8e99c35ad32a32
Hoang Long <litpuvn@gmail.com>
2019-05-23 17:16:27 -0500

    update training

--- lstm_text_generation_training.py
+++ lstm_text_generation_training.py
@@ -65,4 +65,4 @@ filepath="data/model/lstm/weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
 checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
 callbacks_list = [checkpoint]
 # fit the model
-model.fit(X, y, epochs=20, batch_size=128, callbacks=callbacks_list)
+model.fit(X, y, epochs=20, batch_size=8, callbacks=callbacks_list)
